92b6e515b436450a67d80af9388ee8b523bb3215
Arvind Aspengren <arvindaspengren@Arvinds-MacBook-Air.local>
2025-04-19 22:32:48 +0200

    hopefuly fix the users

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://witcuyjpsykbeixvrupt.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpdGN1eWpwc3lrYmVpeHZydXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5OTkzNTQsImV4cCI6MjA2MDU3NTM1NH0.xjPpq1NX1te_qS7n_ulvX5kwSw-_Ayrgx_BaOTMU9Yk"
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  global: {
+    headers: {
+      Authorization: `Bearer ${supabaseAnonKey}`,
+      apikey: supabaseAnonKey,
+    },
+  },
+});
