2443c190954d63c5fe505db0262a811f5ab9f2c1
Arvind Aspengren <arvindaspengren@Arvinds-MacBook-Air.local>
2025-04-21 22:19:53 +0200

    Fix Supabase client config

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,11 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  global: {
-    headers: {
-      Authorization: `Bearer ${supabaseAnonKey}`,
-      apikey: supabaseAnonKey,
-    },
-  },
-});
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
